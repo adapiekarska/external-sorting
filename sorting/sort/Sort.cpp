6286b8792d6d8ca690fd97c3f1580bd2bc460af7
Ada <adapiekarska97@gmail.com>
2018-10-27 17:38:39 +0200

    Work on sorting

--- sorting/sort/Sort.cpp
+++ sorting/sort/Sort.cpp
@@ -15,4 +15,17 @@ void Sort::sort()
 	std::ofstream tape2("tape2", std::ios::binary);
 
 	DataAccessor data_accessor("out");
+
+	while (true)
+	{
+		try
+		{
+			int32_vec v1 = data_accessor.get_next();
+			int32_vec v2 = data_accessor.get_next();
+		}
+		catch (const EOF_Exception&)
+		{
+			break;
+		}
+	}
 }
