dc934f0ccf481206d0daabf540058f0f631dd788
Ada <adapiekarska97@gmail.com>
2018-10-27 18:29:11 +0200

    Overload comparison operators for Int32_Vec

--- sorting/Int32_Vec.h
+++ sorting/Int32_Vec.h
@@ -1,13 +1,24 @@
 #pragma once
 #include <cstdint>
+#include <cmath>
+
 
 class Int32_Vec
 {
 public:
+	Int32_Vec();
 	Int32_Vec(int32_t first, int32_t second);
 	~Int32_Vec();
 
+	friend bool operator> (const Int32_Vec& v1, const Int32_Vec& v2);
+	friend bool operator>= (const Int32_Vec &v1, const Int32_Vec& v2);
+	friend bool operator< (const Int32_Vec& v1, const Int32_Vec& v2);
+	friend bool operator<= (const Int32_Vec& v1, const Int32_Vec& v2);
+
 	int32_t first;
 	int32_t second;
+
+private:
+	unsigned int length;
 };
 
