9274228a6c45614449365d7dbe41486315855d8e
Ada <adapiekarska97@gmail.com>
2018-11-01 21:13:41 +0100

    Extend argument parser

--- sorting/exceptions/EOF_Exception.cpp
+++
@@ -1,7 +0,0 @@
-#include "EOF_Exception.h"
-
-
-const char* EOF_Exception::what() const throw()
-{
-	return "EOF reached.";
-}
--- sorting/main.cpp
+++ sorting/main.cpp
@@ -9,9 +9,10 @@
 
 enum input_mode
 {
-	NONE = -1,
-	IN_FILE = 0,		// randomly generated file
-	IN_USER = 1			// user generated records
+	NONE,
+	IN_RANDOM,		// randomly generated file
+	IN_FILE,		// input file from disk
+	IN_USER			// user generated records
 };
 
 typedef struct CONFIG
@@ -20,6 +21,7 @@ typedef struct CONFIG
 	bool step_by_step;
 	bool verbosity;
 	input_mode input_mode;
+	std::string input_file_path;
 } CONFIG;
 
 CONFIG parse_args(int argc, char** argv)
@@ -29,9 +31,10 @@ CONFIG parse_args(int argc, char** argv)
 	config.step_by_step = false;
 	config.verbosity = false;
 	config.input_mode = NONE;
+	config.input_file_path = "";
 
-	if (argc == 0)
-		config.valid = false;
+	//if (argc == 0)
+	//	config.valid = false;
 
 	std::vector<std::string> opts;
 	for (int i = 1; i < argc; ++i)
@@ -51,23 +54,59 @@ CONFIG parse_args(int argc, char** argv)
 		{
 			config.verbosity = true;
 		}
-		else if ((opt == "-f" || opt == "--file"))
+		else if (opt == "-r" || opt == "--random")
 		{
-			if (config.input_mode == IN_USER)
+			if (config.input_mode == input_mode::IN_USER ||
+				config.input_mode == input_mode::IN_FILE)
 			{
-				std::cout << "Invalid option " << opt << " if -u specified before." << std::endl;
+				std::cout << "Invalid option " << opt << " if config mode "
+					<< config.input_mode << " specified before." << std::endl;
 				config.valid = false;
 			}
 			else
 			{
-				config.input_mode = input_mode::IN_FILE;
+				config.input_mode = input_mode::IN_RANDOM;
+			}
+		}
+		else if (opt == "-f" || opt == "--file")
+		{
+			if (i != opts.size() - 1)
+			{
+				if (config.input_mode == input_mode::IN_USER || 
+					config.input_mode == input_mode::IN_RANDOM)
+				{
+					std::cout << "Invalid option " << opt << " if config mode "
+						<< config.input_mode << " specified before." << std::endl;
+					config.valid = false;
+				}
+				else
+				{
+					// omit the file path in argument parsing
+					config.input_mode = input_mode::IN_FILE;
+					if (opts[i + 1][0] != '-')
+					{
+						config.input_file_path = opts[i + 1];
+						i++;
+					}
+					else
+					{
+						std::cout << "Missing filename after " << opt << std::endl;
+						config.valid = false;
+					}
+				}
+			}
+			else
+			{
+				config.valid = false;
 			}
 		}
 		else if (opt == "-u" || opt == "--user")
 		{
-			if (config.input_mode == input_mode::IN_FILE)
+			if (config.input_mode == input_mode::IN_FILE ||
+				config.input_mode == input_mode::IN_RANDOM)
 			{
-				std::cout << "Invalid option " << opt << " if -f specified before." << std::endl;
+				std::cout << "Invalid option " << opt << " if config mode "
+					<< config.input_mode << " specified before." << std::endl;
 				config.valid = false;
 			}
 			else
@@ -77,7 +116,30 @@ CONFIG parse_args(int argc, char** argv)
 		}
 		else if (opt == "-h" || opt == "--help")
 		{
-
+			std::cout << 
+				"Options:\n"
+				"\n"
+				"	-r, --random\n"
+				"		Randomly generates input file.\n"
+				"\n"
+				"	-f [FILE_PATH], --file [FILE_PATH]\n"
+				"		By supplying this option, the user is allowed to specify a path to the\n"
+				"		binary input file.\n"
+				"\n"
+				"	-u, --user\n"
+				"		Guides user through the process of typing the records with the keyboard.\n"
+				"\n"
+				"	If no one of the above flags is specified, the input is generated randomly\n"
+				"	just as if -r flag was provided.\n"
+				"\n"
+				"	-s, --step-by-step\n"
+				"		Lets user go through sorting step by step. This option also sets maximum\n"
+				"		verbosity.\n"
+				"\n"
+				"	-v, --verbose\n"
+				"		Sets maximum verbosity level. This means displaying each tape during the\n"
+				"		sorting process.\n";
+			config.valid = false;
 		}
 		else
 		{
@@ -86,6 +148,10 @@ CONFIG parse_args(int argc, char** argv)
 		}
 		i++;
 	}
+
+	if (config.input_mode == input_mode::NONE)
+		config.input_mode = input_mode::IN_RANDOM;
+
 	return config;
 }
 
@@ -102,16 +168,34 @@ int main(int argc, char** argv)
 
 	DataGenerator data_generator;
 	std::vector<Int32_Vec> data;
-	if (config.input_mode == IN_USER)
-		data = data_generator.user_generate();
-	else if (config.input_mode == IN_FILE)
-		data = data_generator.random_generate(8, 0, 10);
+	std::string input_file_name;
 
-	FileGenerator file_generator("data");
-	file_generator.write(data);
+	switch (config.input_mode)
+	{
+		case input_mode::IN_RANDOM:
+		{
+			data = data_generator.random_generate(8, 0, 10);
+			input_file_name = "data";
+			FileGenerator file_generator(input_file_name);
+			file_generator.write(data);
+		}
+		break;
+		case input_mode::IN_FILE:
+			input_file_name = config.input_file_path;
+		break;
+		case input_mode::IN_USER:
+		{
+			data = data_generator.user_generate();
+			input_file_name = "data";
+			FileGenerator file_generator(input_file_name);
+			file_generator.write(data);
+		}
+		break;
+	}
 
-	Sorter sorter("data5");
-	sorter.sort(config.step_by_step, config.verbosity);
+	Sorter sorter(input_file_name);
+	//Sorter sorter("data5");
+	//sorter.sort(config.step_by_step, config.verbosity);
 
 	/*
 	DataGenerator data_generator;
